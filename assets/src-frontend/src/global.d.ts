5ce08847ef7d7fbf20f15cd3a523f40973ff157a
Vladimir Adamic <vladimir.adamic@gmail.com>
2020-12-09 10:45:52 +0100

    T-52871: Add some of global types
    - forbid usage of window.SF
    
    Change-Id: I9d45f1d1620179ca1dee99bd479211492b402b3c

---
+++ assets/src-frontend/src/global.d.ts
@@ -0,0 +1,54 @@
+interface SF {
+	params: VfParams;
+	decodeEntities(): Record<string, unknown> | string;
+	decodeObject(obj: Record<string, unknown>): Record<string, unknown>;
+	// TODO: infer type dynamically from a RemoteAction
+	invokeAction(
+		remoteActionName: 'getAppSettings',
+		parametersArr?: [string]
+	): Promise<AppSettings>;
+	param: {
+		account: string;
+	};
+	apiSession: string;
+	actions: RemoteActions;
+	labels: Record<string, string>;
+	fieldLabels: {};
+}
+
+interface Window {
+	/* eslint-disable @typescript-eslint/naming-convention */
+
+	/**
+	 * @deprecated
+	 */
+	SF: SF;
+	/* eslint-enable @typescript-eslint/naming-convention */
+}
+
+type VfParams = {
+	account: string;
+};
+
+interface RemoteActions {
+	getAppSettings(paramsArray: [string]): Promise<AppSettings>;
+	// TODO: define all actions
+}
+
+interface Account {
+	Id: string;
+	Name: string;
+}
+
+// TODO define the unknowns
+interface AppSettings {
+	account: Account;
+	HeaderData: Record<string, unknown>;
+	CustomTabsData: Record<string, unknown>;
+	ButtonCustomData: Record<string, unknown>;
+	ButtonStandardData: Record<string, unknown>;
+	RelatedListsData: Record<string, unknown>;
+	AddonCategorizationData: Record<string, unknown>;
+	CategorizationData: Record<string, unknown>;
+	FACSettings: Record<string, Record<string, unknown>>;
+}

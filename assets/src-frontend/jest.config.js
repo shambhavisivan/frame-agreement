65e6f5b8f624b2f325724d7c508e5b90b15453bb
Vladimir Adamic <vladimir.adamic@gmail.com>
2021-01-07 14:54:28 +0100

    T-52871: Remove coverage threshold
    It would be unproductive insisting on having tests for state
    management and components at this point. The presentational
    components will be swapped with cs-ui components which would invalidate
    any test anyway. Also, the final structure of the application state is
    unknown and by locking it down with reducer tests it would make
    it much harder to evolve towards an optimal structure as each change
    would require updating all of the reducer use-cases.
    
    Change-Id: I2c94e0542cca20a61dd9b93d5ef090a720ef3749

--- assets/src-frontend/jest.config.js
+++ assets/src-frontend/jest.config.js
@@ -7,13 +7,6 @@ module.exports = {
 	setupFiles: ['<rootDir>/src/local-server/local_data.js'],
 	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)?$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	coverageThreshold: {
-		global: {
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: -10
-		}
-	}
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
+

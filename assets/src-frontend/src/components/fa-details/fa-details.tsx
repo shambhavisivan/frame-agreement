721867c8a318c2f92cdb7450c1c2866675ec694c
Vladimir Adamic <vladimir.adamic@gmail.com>
2021-01-08 12:34:35 +0100

    T-52871: Use template literal for route param types
    Change-Id: Ife26c9d708614e2875e876d9978fbe549e8916bf

--- assets/src-frontend/src/components/fa-details/fa-details.tsx
+++ assets/src-frontend/src/components/fa-details/fa-details.tsx
@@ -1,11 +1,9 @@
 import React, { ReactElement } from 'react';
-import { useParams } from 'react-router';
 import { useFrameAgreements } from '../../hooks/use-frame-agreements';
 import { LoadingFallback } from '../loading-fallback';
 import { FaEditor } from './fa-editor';
 
-export function FrameAgreementDetails(): ReactElement {
-	const { agreementId } = useParams<{ agreementId: string }>();
+export function FrameAgreementDetails({ agreementId }: { agreementId: string }): ReactElement {
 	const { agreements = [], status: faStatus } = useFrameAgreements();
 	const agreement = agreements.find((a) => a.id === agreementId);
 
--- assets/src-frontend/src/components/pages.tsx
+++ assets/src-frontend/src/components/pages.tsx
@@ -1,22 +1,44 @@
 import React, { ReactElement } from 'react';
-import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch, useParams } from 'react-router-dom';
 import { FrameAgreementDetails } from './fa-details';
 import { FrameAgreementList } from './fa-list';
 import { NotFound } from './not-found';
+
+type ParseRouteParameters<Route> = Route extends `${string}/:${infer Param}/${infer Rest}`
+	? { [Entry in Param | keyof ParseRouteParameters<`/${Rest}`>]: string }
+	: Route extends `${string}/:${infer Param}`
+	? { [Entry in Param]: string }
+	: Record<string, unknown>;
+
+type FaDetailsRoute = `/agreement/:agreementId`;
+type FaListRoute = `/agreement`;
+
+const frameAgreementDetailsRoute: FaDetailsRoute = '/agreement/:agreementId';
+const frameAgreementListRoute: FaListRoute = '/agreement';
 
 export function Pages(): ReactElement {
 	return (
 		<Router>
 			<Switch>
-				<Route path={`/agreement/:agreementId`}>
-					<FrameAgreementDetails />
+				<Route path={frameAgreementDetailsRoute}>
+					<FrameAgreementDetailsPage />
 				</Route>
-				<Route path="/agreement">
-					<FrameAgreementList />
+				<Route path={frameAgreementListRoute}>
+					<FrameAgreementListPage />
 				</Route>
 				<Redirect exact from="/" to="/agreement" />
 				<Route path="*" component={NotFound} />
 			</Switch>
 		</Router>
 	);
+}
+
+function FrameAgreementDetailsPage(): ReactElement {
+	const { agreementId } = useParams<ParseRouteParameters<FaDetailsRoute>>();
+
+	return <FrameAgreementDetails agreementId={agreementId} />;
+}
+
+function FrameAgreementListPage(): ReactElement {
+	return <FrameAgreementList />;
 }

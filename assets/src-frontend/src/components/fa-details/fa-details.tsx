722a48a6bbf7589b8ae99d54ca02013fc4dc9d53
Dominik Kralj <dominik.kralj@cloudsense.com>
2021-06-07 15:04:17 +0200

    T-54141: Add approval history styling
    Change-Id: I63cee6bd94e0cc635b92ec060e951a9bf6567f6d

--- assets/src-frontend/src/components/fa-details/fa-details.tsx
+++ assets/src-frontend/src/components/fa-details/fa-details.tsx
@@ -2,6 +2,19 @@ import React, { ReactElement } from 'react';
 import { useFrameAgreements } from '../../hooks/use-frame-agreements';
 import { LoadingFallback } from '../loading-fallback';
 import { FaEditor } from './fa-editor';
+import {
+	CSCard,
+	CSCardHeader,
+	CSCardBody,
+	CSTextarea,
+	CSButton,
+	CSButtonGroup,
+	CSTable,
+	CSTableBody,
+	CSTableCell,
+	CSTableHeader,
+	CSTableRow
+} from '@cloudsense/cs-ui-components';
 
 interface FrameAgreementDetailsProps {
 	agreementId: string;
@@ -15,6 +28,57 @@ export function FrameAgreementDetails({ agreementId }: FrameAgreementDetailsProp
 		<div className="details-wrapper">
 			<LoadingFallback status={faStatus}>
 				<div className="field-wrapper"> </div>
+				<CSCard className="approval-history-wrapper">
+					<CSCardHeader title="Approval history" collapsible defaultClosed />
+					<CSCardBody padding="0.75rem 0 0">
+						<CSTextarea
+							label="Comment"
+							placeholder="Enter comment..."
+							borderRadius="0.25rem"
+						/>
+						<CSButtonGroup ariaDescription="approval actions">
+							<CSButton label="Recall" iconName="undo" />
+							<CSButton label="Approve" iconName="like" />
+							<CSButton label="Reject" iconName="dislike" />
+						</CSButtonGroup>
+						<CSTable selectableRows>
+							<CSTableHeader>
+								<CSTableCell text="Action" grow={2} />
+								<CSTableCell text="Date" grow={1} />
+								<CSTableCell text="Status" grow={1} />
+								<CSTableCell text="Assigned to" grow={1} />
+								<CSTableCell text="Actual Approver" grow={1} />
+								<CSTableCell text="Comments" grow={1} />
+							</CSTableHeader>
+							<CSTableBody>
+								<CSTableRow>
+									<CSTableCell text="Step1" grow={2} />
+									<CSTableCell text="03/05/2019 12:20" grow={1} />
+									<CSTableCell text="Pending" grow={1} />
+									<CSTableCell text="International - Platinum/Gold" grow={1} />
+									<CSTableCell text="International - Platinum/Gold" grow={1} />
+									<CSTableCell text="Submitted frame agreement" grow={1} />
+								</CSTableRow>
+								<CSTableRow>
+									<CSTableCell text="Step1" grow={2} />
+									<CSTableCell text="03/05/2019 12:20" grow={1} />
+									<CSTableCell text="Pending" grow={1} />
+									<CSTableCell text="International - Platinum/Gold" grow={1} />
+									<CSTableCell text="International - Platinum/Gold" grow={1} />
+									<CSTableCell text="Submitted frame agreement" grow={1} />
+								</CSTableRow>
+								<CSTableRow>
+									<CSTableCell text="Step1" grow={2} />
+									<CSTableCell text="03/05/2019 12:20" grow={1} />
+									<CSTableCell text="Pending" grow={1} />
+									<CSTableCell text="International - Platinum/Gold" grow={1} />
+									<CSTableCell text="International - Platinum/Gold" grow={1} />
+									<CSTableCell text="Submitted frame agreement" grow={1} />
+								</CSTableRow>
+							</CSTableBody>
+						</CSTable>
+					</CSCardBody>
+				</CSCard>
 				Agreement ID: {agreementId}, name: {agreement?.name}
 				<FaEditor agreement={agreement} />
 			</LoadingFallback>
